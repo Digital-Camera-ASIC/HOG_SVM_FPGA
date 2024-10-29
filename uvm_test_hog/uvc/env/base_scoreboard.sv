c08cd4490b183f061c519209fc4ed7a1b733b9ab
datce2003 <datce2003@gmail.com>
2024-10-29 14:51:58 +0700

    complete extract feature from driver

--- uvm_test_hog/uvc/env/base_scoreboard.sv
+++ uvm_test_hog/uvc/env/base_scoreboard.sv
@@ -6,12 +6,14 @@ class base_scoreboard extends uvm_scoreboard;
   uvm_analysis_imp_mon #(base_item, base_scoreboard) mon_item_collected_export;
   `uvm_component_utils(base_scoreboard)
 
+  parameter real pi = 3.141592653589793;
+  
   logic [42][767:0] fifo;
 
-  logic [767:0] bin_1;
-  logic [767:0] bin_2;
-  logic [767:0] bin_3;
-  logic [767:0] bin_4;
+  logic [767:0] data_1;
+  logic [767:0] data_2;
+  logic [767:0] data_3;
+  logic [767:0] data_4;
 
   real Gx_1[64];
   real Gx_2[64];
@@ -33,6 +35,10 @@ class base_scoreboard extends uvm_scoreboard;
   real Orientation_3[64];
   real Orientation_4[64];
 
+  real bin_1[9];
+  real bin_2[9];
+  real bin_3[9];
+  real bin_4[9];
 
 
   real temp_fea_a[9];
@@ -60,42 +66,44 @@ class base_scoreboard extends uvm_scoreboard;
     super.new(name, parent);
   endfunction
 
-  function void extract_feature(logic [767:0] bin_1, logic [767:0] bin_2, logic [767:0] bin_3,
-                                logic [767:0] bin_4);
-    $display($sformatf("bin_1: %d, bin_2: %d, bin_3: %d, bin_4: %d", bin_1, bin_2, bin_3, bin_4));
+  function void extract_feature(logic [767:0] data_1, logic [767:0] data_2, logic [767:0] data_3,
+                                logic [767:0] data_4);
+    $display($sformatf("data_1: %d, data_2: %d, data_3: %d, data_4: %d", data_1, data_2, data_3, data_4));
 
-    /*  DEBUG --------------------- 
-    // $display($sformatf("x: %h, y: %h, z: %h, t: %h", bin_1[8*0+:8], bin_2[8*0+:8], bin_3[8*0+:8], bin_4[8*0+:8]));
+    //  DEBUG --------------------- 
+    // $display($sformatf("x: %h, y: %h, z: %h, t: %h", data_1[8*0+:8], data_2[8*0+:8], data_3[8*0+:8], data_4[8*0+:8]));
 
-    // $display($sformatf("x: %h, y: %h", bin_1[8*2+:8], bin_1[8*0+:8]));
-    // $display($sformatf("bieu thuc x - y: %f", (bin_1[8*2+:8] - bin_1[8*0+:8]) * 1.0 ));
+    // $display($sformatf("x: %h, y: %h", data_1[8*2+:8], data_1[8*0+:8]));
+    // $display($sformatf("bieu thuc x - y: %f", (data_1[8*2+:8] - data_1[8*0+:8]) * 1.0 ));
 
-    // $display($sformatf("x: %h, y: %h", bin_2[8*2+:8], bin_2[8*0+:8]));
-    // $display($sformatf("bieu thuc x - y: %f", (bin_2[8*2+:8]  - bin_2[8*0+:8]) * 1.0));
+    // $display($sformatf("x: %h, y: %h", data_2[8*2+:8], data_2[8*0+:8]));
+    // $display($sformatf("bieu thuc x - y: %f", (data_2[8*2+:8]  - data_2[8*0+:8]) * 1.0));
 
-    // $display($sformatf("x: %h, y: %h", bin_3[8*2+:8], bin_3[8*0+:8]));
-    // $display($sformatf("bieu thuc x - y: %f", bin_3[8*2+:8] * 1.0 - bin_3[8*0+:8]));
+    // $display($sformatf("x: %h, y: %h", data_3[8*2+:8], data_3[8*0+:8]));
+    // $display($sformatf("bieu thuc x - y: %f", data_3[8*2+:8] * 1.0 - data_3[8*0+:8]));
 
-    // $display($sformatf("x: %h, y: %h", bin_4[8*2+:8], bin_4[8*0+:8]));
-    // $display($sformatf("bieu thuc x - y: %f", bin_4[8*2+:8] * 1.0 - bin_4[8*0+:8]));
+    // $display($sformatf("x: %h, y: %h", data_4[8*2+:8], data_4[8*0+:8]));
+    // $display($sformatf("bieu thuc x - y: %f", data_4[8*2+:8] * 1.0 - data_4[8*0+:8]));
 
-     ---------------------         */ 
+
+      $display($sformatf("atan2(gy,gx) : %f", $atan2(-41, 0.01) * 180/ pi));
+     //---------------------         
 
 
     // sum = 0;
     // for (int i = 0; i < 288; i = i + 32) begin
-    //   sum = sum + 1.0 * (bin_1[i+:32]) / 2 ** 16;
-    //   sum = sum + 1.0 * (bin_2[i+:32]) / 2 ** 16;
-    //   sum = sum + 1.0 * (bin_3[i+:32]) / 2 ** 16;
-    //   sum = sum + 1.0 * (bin_4[i+:32]) / 2 ** 16;
+    //   sum = sum + 1.0 * (data_1[i+:32]) / 2 ** 16;
+    //   sum = sum + 1.0 * (data_2[i+:32]) / 2 ** 16;
+    //   sum = sum + 1.0 * (data_3[i+:32]) / 2 ** 16;
+    //   sum = sum + 1.0 * (data_4[i+:32]) / 2 ** 16;
     // end
     // $display("index: %0d", cnt_debug++);
     // $display("Sum : %f", sum);
     // for (int i = 0; i < 288; i = i + 32) begin
-    //   temp_fea_a[i/32] = $sqrt((1.0 * (bin_1[i+:32]) / 2 ** 16) / sum);
-    //   temp_fea_b[i/32] = $sqrt((1.0 * (bin_2[i+:32]) / 2 ** 16) / sum);
-    //   temp_fea_c[i/32] = $sqrt((1.0 * (bin_3[i+:32]) / 2 ** 16) / sum);
-    //   temp_fea_d[i/32] = $sqrt((1.0 * (bin_4[i+:32]) / 2 ** 16) / sum);
+    //   temp_fea_a[i/32] = $sqrt((1.0 * (data_1[i+:32]) / 2 ** 16) / sum);
+    //   temp_fea_b[i/32] = $sqrt((1.0 * (data_2[i+:32]) / 2 ** 16) / sum);
+    //   temp_fea_c[i/32] = $sqrt((1.0 * (data_3[i+:32]) / 2 ** 16) / sum);
+    //   temp_fea_d[i/32] = $sqrt((1.0 * (data_4[i+:32]) / 2 ** 16) / sum);
     // end
     // q_fea_a_golden.push_back(temp_fea_a);
     // q_fea_b_golden.push_back(temp_fea_b);
@@ -103,7 +111,7 @@ class base_scoreboard extends uvm_scoreboard;
     // q_fea_d_golden.push_back(temp_fea_d);
     // $display($sformatf("--------------------"));
 
-    // $display($sformatf("Bin_1: %h, Bin_2: %h, Bin_3: %h, Bin_4: %h", bin_1, bin_2, bin_3, bin_4));
+    // $display($sformatf("data_1: %h, data_2: %h, data_3: %h, data_4: %h", data_1, data_2, data_3, data_4));
 
     // for (int i = 0; i < 9; i++) begin
     //   $display($sformatf("feature a[%0d]: %f", i, temp_fea_a[i]));
@@ -121,86 +129,86 @@ class base_scoreboard extends uvm_scoreboard;
     // $display($sformatf("--------------------"));
 
 
-    // Gx_1[0] = bin_1[1*8+:8] - bin_1[80*8+:8];
-    // Gx_1[8] = bin_1[9*8+:8] - bin_1[81*8+:8];
-    // Gx_1[16] = bin_1[17*8+:8] - bin_1[82*8+:8];
-    // Gx_1[24] = bin_1[25*8+:8] - bin_1[83*8+:8];
-    // Gx_1[32] = bin_1[33*8+:8] - bin_1[84*8+:8];
-    // Gx_1[40] = bin_1[41*8+:8] - bin_1[85*8+:8];
-    // Gx_1[48] = bin_1[49*8+:8] - bin_1[86*8+:8];
-    // Gx_1[56] = bin_1[57*8+:8] - bin_1[87*8+:8];
+    // Gx_1[0] = data_1[1*8+:8] - data_1[80*8+:8];
+    // Gx_1[8] = data_1[9*8+:8] - data_1[81*8+:8];
+    // Gx_1[16] = data_1[17*8+:8] - data_1[82*8+:8];
+    // Gx_1[24] = data_1[25*8+:8] - data_1[83*8+:8];
+    // Gx_1[32] = data_1[33*8+:8] - data_1[84*8+:8];
+    // Gx_1[40] = data_1[41*8+:8] - data_1[85*8+:8];
+    // Gx_1[48] = data_1[49*8+:8] - data_1[86*8+:8];
+    // Gx_1[56] = data_1[57*8+:8] - data_1[87*8+:8];
 
-    // Gx_1[7] = bin_1[88*8+:8] - bin_1[6*8+:8];
-    // Gx_1[15] = bin_1[89*8+:8] - bin_1[14*8+:8];
-    // Gx_1[23] = bin_1[90*8+:8] - bin_1[22*8+:8];
-    // Gx_1[31] = bin_1[91*8+:8] - bin_1[30*8+:8];
-    // Gx_1[39] = bin_1[92*8+:8] - bin_1[38*8+:8];
-    // Gx_1[47] = bin_1[93*8+:8] - bin_1[46*8+:8];
-    // Gx_1[55] = bin_1[94*8+:8] - bin_1[54*8+:8];
-    // Gx_1[63] = bin_1[95*8+:8] - bin_1[62*8+:8];
+    // Gx_1[7] = data_1[88*8+:8] - data_1[6*8+:8];
+    // Gx_1[15] = data_1[89*8+:8] - data_1[14*8+:8];
+    // Gx_1[23] = data_1[90*8+:8] - data_1[22*8+:8];
+    // Gx_1[31] = data_1[91*8+:8] - data_1[30*8+:8];
+    // Gx_1[39] = data_1[92*8+:8] - data_1[38*8+:8];
+    // Gx_1[47] = data_1[93*8+:8] - data_1[46*8+:8];
+    // Gx_1[55] = data_1[94*8+:8] - data_1[54*8+:8];
+    // Gx_1[63] = data_1[95*8+:8] - data_1[62*8+:8];
 
     for (int i = 0; i < 8; i++) begin
-      Gx_1[i*8] = 1.0 * bin_1[(i*8 + 1)*8+:8] - 1.0 * bin_1[(80 + i)*8+:8];
-      Gx_2[i*8] = 1.0 * bin_2[(i*8 + 1)*8+:8] - 1.0 * bin_2[(80 + i)*8+:8];
-      Gx_3[i*8] = 1.0 * bin_3[(i*8 + 1)*8+:8] - 1.0 * bin_3[(80 + i)*8+:8];
-      Gx_4[i*8] = 1.0 * bin_4[(i*8 + 1)*8+:8] - 1.0 * bin_4[(80 + i)*8+:8];
+      Gx_1[i*8] = 1.0 * data_1[(i*8 + 1)*8+:8] - 1.0 * data_1[(80 + i)*8+:8];
+      Gx_2[i*8] = 1.0 * data_2[(i*8 + 1)*8+:8] - 1.0 * data_2[(80 + i)*8+:8];
+      Gx_3[i*8] = 1.0 * data_3[(i*8 + 1)*8+:8] - 1.0 * data_3[(80 + i)*8+:8];
+      Gx_4[i*8] = 1.0 * data_4[(i*8 + 1)*8+:8] - 1.0 * data_4[(80 + i)*8+:8];
     end
 
     for (int i = 0; i < 8; i++) begin
-      Gx_1[i*8 + 7] = 1.0 * bin_1[(88 + i)*8+:8] - 1.0 * bin_1[(6 + i*8)*8+:8];
-      Gx_2[i*8 + 7] = 1.0 * bin_2[(88 + i)*8+:8] - 1.0 * bin_2[(6 + i*8)*8+:8];
-      Gx_3[i*8 + 7] = 1.0 * bin_3[(88 + i)*8+:8] - 1.0 * bin_3[(6 + i*8)*8+:8];
-      Gx_4[i*8 + 7] = 1.0 * bin_4[(88 + i)*8+:8] - 1.0 * bin_4[(6 + i*8)*8+:8];
+      Gx_1[i*8 + 7] = 1.0 * data_1[(88 + i)*8+:8] - 1.0 * data_1[(6 + i*8)*8+:8];
+      Gx_2[i*8 + 7] = 1.0 * data_2[(88 + i)*8+:8] - 1.0 * data_2[(6 + i*8)*8+:8];
+      Gx_3[i*8 + 7] = 1.0 * data_3[(88 + i)*8+:8] - 1.0 * data_3[(6 + i*8)*8+:8];
+      Gx_4[i*8 + 7] = 1.0 * data_4[(88 + i)*8+:8] - 1.0 * data_4[(6 + i*8)*8+:8];
     end
 
     for (int i = 0; i < 8; i ++) begin
       for (int j = 1; j < 7; j++) begin
-        Gx_1[i * 8 + j] = 1.0 * bin_1[((i*8+j + 1)*8) +: 8] - 1.0 * bin_1[((i*8+j - 1)*8) +: 8];
-        Gx_2[i * 8 + j] = 1.0 * bin_2[((i*8+j + 1)*8) +: 8] - 1.0 * bin_2[((i*8+j - 1)*8) +: 8];
-        Gx_3[i * 8 + j] = 1.0 * bin_3[((i*8+j + 1)*8) +: 8] - 1.0 * bin_3[((i*8+j - 1)*8) +: 8];
-        Gx_4[i * 8 + j] = 1.0 * bin_4[((i*8+j + 1)*8) +: 8] - 1.0 * bin_4[((i*8+j - 1)*8) +: 8];
+        Gx_1[i * 8 + j] = 1.0 * data_1[((i*8+j + 1)*8) +: 8] - 1.0 * data_1[((i*8+j - 1)*8) +: 8];
+        Gx_2[i * 8 + j] = 1.0 * data_2[((i*8+j + 1)*8) +: 8] - 1.0 * data_2[((i*8+j - 1)*8) +: 8];
+        Gx_3[i * 8 + j] = 1.0 * data_3[((i*8+j + 1)*8) +: 8] - 1.0 * data_3[((i*8+j - 1)*8) +: 8];
+        Gx_4[i * 8 + j] = 1.0 * data_4[((i*8+j + 1)*8) +: 8] - 1.0 * data_4[((i*8+j - 1)*8) +: 8];
       end
     end
 
 
-    // Gy_1[0] = bin_1[8*8+:8] - bin_1[64*8+:8];
-    // Gy_1[1] = bin_1[9*8+:8] - bin_1[65*8+:8];
-    // Gy_1[2] = bin_1[10*8+:8] - bin_1[66*8+:8];
-    // Gy_1[3] = bin_1[11*8+:8] - bin_1[67*8+:8];
-    // Gy_1[4] = bin_1[12*8+:8] - bin_1[68*8+:8];
-    // Gy_1[5] = bin_1[13*8+:8] - bin_1[69*8+:8];
-    // Gy_1[6] = bin_1[14*8+:8] - bin_1[70*8+:8];
-    // Gy_1[7] = bin_1[15*8+:8] - bin_1[71*8+:8];
+    // Gy_1[0] = data_1[8*8+:8] - data_1[64*8+:8];
+    // Gy_1[1] = data_1[9*8+:8] - data_1[65*8+:8];
+    // Gy_1[2] = data_1[10*8+:8] - data_1[66*8+:8];
+    // Gy_1[3] = data_1[11*8+:8] - data_1[67*8+:8];
+    // Gy_1[4] = data_1[12*8+:8] - data_1[68*8+:8];
+    // Gy_1[5] = data_1[13*8+:8] - data_1[69*8+:8];
+    // Gy_1[6] = data_1[14*8+:8] - data_1[70*8+:8];
+    // Gy_1[7] = data_1[15*8+:8] - data_1[71*8+:8];
 
     for (int i = 0; i < 8; i++) begin
-      Gy_1[i] = 1.0 * bin_1[(8 + i)*8+:8] - 1.0 * bin_1[(64 + i)*8+:8];
-      Gy_2[i] = 1.0 * bin_2[(8 + i)*8+:8] - 1.0 * bin_2[(64 + i)*8+:8];
-      Gy_3[i] = 1.0 * bin_3[(8 + i)*8+:8] - 1.0 * bin_3[(64 + i)*8+:8];
-      Gy_4[i] = 1.0 * bin_4[(8 + i)*8+:8] - 1.0 * bin_4[(64 + i)*8+:8];
+      Gy_1[i] = 1.0 * data_1[(8 + i)*8+:8] - 1.0 * data_1[(64 + i)*8+:8];
+      Gy_2[i] = 1.0 * data_2[(8 + i)*8+:8] - 1.0 * data_2[(64 + i)*8+:8];
+      Gy_3[i] = 1.0 * data_3[(8 + i)*8+:8] - 1.0 * data_3[(64 + i)*8+:8];
+      Gy_4[i] = 1.0 * data_4[(8 + i)*8+:8] - 1.0 * data_4[(64 + i)*8+:8];
     end
 
-    // Gy_1[56] = bin_1[72*8+:8] - bin_1[56*8+:8];
-    // Gy_1[57] = bin_1[73*8+:8] - bin_1[57*8+:8];
-    // Gy_1[58] = bin_1[74*8+:8] - bin_1[58*8+:8];
-    // Gy_1[59] = bin_1[75*8+:8] - bin_1[59*8+:8];
-    // Gy_1[60] = bin_1[76*8+:8] - bin_1[60*8+:8];
-    // Gy_1[61] = bin_1[77*8+:8] - bin_1[61*8+:8];
-    // Gy_1[62] = bin_1[78*8+:8] - bin_1[62*8+:8];
-    // Gy_1[63] = bin_1[79*8+:8] - bin_1[63*8+:8];
+    // Gy_1[56] = data_1[72*8+:8] - data_1[56*8+:8];
+    // Gy_1[57] = data_1[73*8+:8] - data_1[57*8+:8];
+    // Gy_1[58] = data_1[74*8+:8] - data_1[58*8+:8];
+    // Gy_1[59] = data_1[75*8+:8] - data_1[59*8+:8];
+    // Gy_1[60] = data_1[76*8+:8] - data_1[60*8+:8];
+    // Gy_1[61] = data_1[77*8+:8] - data_1[61*8+:8];
+    // Gy_1[62] = data_1[78*8+:8] - data_1[62*8+:8];
+    // Gy_1[63] = data_1[79*8+:8] - data_1[63*8+:8];
 
     for (int i = 0; i < 8; i++) begin
-      Gy_1[56 + i] = 1.0 * bin_1[(72 + i)*8+:8] - 1.0 * bin_1[(56 + i)*8+:8];
-      Gy_2[56 + i] = 1.0 * bin_2[(72 + i)*8+:8] - 1.0 * bin_2[(56 + i)*8+:8];
-      Gy_3[56 + i] = 1.0 * bin_3[(72 + i)*8+:8] - 1.0 * bin_3[(56 + i)*8+:8];
-      Gy_4[56 + i] = 1.0 * bin_4[(72 + i)*8+:8] - 1.0 * bin_4[(56 + i)*8+:8];
+      Gy_1[56 + i] = 1.0 * data_1[(72 + i)*8+:8] - 1.0 * data_1[(56 + i)*8+:8];
+      Gy_2[56 + i] = 1.0 * data_2[(72 + i)*8+:8] - 1.0 * data_2[(56 + i)*8+:8];
+      Gy_3[56 + i] = 1.0 * data_3[(72 + i)*8+:8] - 1.0 * data_3[(56 + i)*8+:8];
+      Gy_4[56 + i] = 1.0 * data_4[(72 + i)*8+:8] - 1.0 * data_4[(56 + i)*8+:8];
     end
 
     for (int i = 1; i < 7; i++) begin
       for (int j = 0; j < 8; j++) begin
-        Gy_1[i*8 + j] = 1.0 * bin_1[((i*8 + j + 8)*8) +: 8] - 1.0 * bin_1[((i*8 + j - 8)*8) +: 8];
-        Gy_2[i*8 + j] = 1.0 * bin_2[((i*8 + j + 8)*8) +: 8] - 1.0 * bin_2[((i*8 + j - 8)*8) +: 8];
-        Gy_3[i*8 + j] = 1.0 * bin_3[((i*8 + j + 8)*8) +: 8] - 1.0 * bin_3[((i*8 + j - 8)*8) +: 8];
-        Gy_4[i*8 + j] = 1.0 * bin_4[((i*8 + j + 8)*8) +: 8] - 1.0 * bin_4[((i*8 + j - 8)*8) +: 8];
+        Gy_1[i*8 + j] = 1.0 * data_1[((i*8 + j + 8)*8) +: 8] - 1.0 * data_1[((i*8 + j - 8)*8) +: 8];
+        Gy_2[i*8 + j] = 1.0 * data_2[((i*8 + j + 8)*8) +: 8] - 1.0 * data_2[((i*8 + j - 8)*8) +: 8];
+        Gy_3[i*8 + j] = 1.0 * data_3[((i*8 + j + 8)*8) +: 8] - 1.0 * data_3[((i*8 + j - 8)*8) +: 8];
+        Gy_4[i*8 + j] = 1.0 * data_4[((i*8 + j + 8)*8) +: 8] - 1.0 * data_4[((i*8 + j - 8)*8) +: 8];
       end
     end
 
@@ -219,11 +227,54 @@ class base_scoreboard extends uvm_scoreboard;
     end
 
     for (int i = 0; i < 64; i++) begin
-      Orientation_1[i] = $atan2(Gy_1[i], Gx_1[i]);
-      Orientation_2[i] = $atan2(Gy_2[i], Gx_2[i]);
-      Orientation_3[i] = $atan2(Gy_3[i], Gx_3[i]);
-      Orientation_4[i] = $atan2(Gy_4[i], Gx_4[i]);
+      Orientation_1[i] = ($atan2(Gy_1[i], Gx_1[i]) * 180 / pi >= 0) ? $atan2(Gy_1[i], Gx_1[i]) * 180 / pi : $atan2(Gy_1[i], Gx_1[i]) * 180 / pi + 180;
+      Orientation_2[i] = ($atan2(Gy_2[i], Gx_2[i]) * 180 / pi >= 0) ? $atan2(Gy_2[i], Gx_2[i]) * 180 / pi : $atan2(Gy_2[i], Gx_2[i]) * 180 / pi + 180;
+      Orientation_3[i] = ($atan2(Gy_3[i], Gx_3[i]) * 180 / pi >= 0) ? $atan2(Gy_3[i], Gx_3[i]) * 180 / pi : $atan2(Gy_3[i], Gx_3[i]) * 180 / pi + 180;
+      Orientation_4[i] = ($atan2(Gy_4[i], Gx_4[i]) * 180 / pi >= 0) ? $atan2(Gy_4[i], Gx_4[i]) * 180 / pi : $atan2(Gy_4[i], Gx_4[i]) * 180 / pi + 180;
     end
+
+    for (int i = 0; i < 9; i++) begin
+      bin_1[i] = 0;
+      bin_2[i] = 0;
+      bin_3[i] = 0;
+      bin_4[i] = 0;
+    end
+    
+    for (int i = 0; i < 64; i++) begin
+      bin_1[(int'(Orientation_1[i]) / 20) % 9] += Magnitude_1[i];
+      bin_2[(int'(Orientation_2[i]) / 20) % 9] += Magnitude_2[i];
+      bin_3[(int'(Orientation_3[i]) / 20) % 9] += Magnitude_3[i];
+      bin_4[(int'(Orientation_4[i]) / 20) % 9] += Magnitude_4[i];
+    end
+
+    for (int i = 0; i < 64; i++) begin
+      $display($sformatf("Magnitude_1 [%0d]: %f, Orientation_1 [%0d]: %f", i, Magnitude_1[i], i, Orientation_1[i]));
+      $display($sformatf("Magnitude_2 [%0d]: %f, Orientation_2 [%0d]: %f", i, Magnitude_2[i], i, Orientation_2[i]));
+      $display($sformatf("Magnitude_3 [%0d]: %f, Orientation_3 [%0d]: %f", i, Magnitude_3[i], i, Orientation_3[i]));
+      $display($sformatf("Magnitude_4 [%0d]: %f, Orientation_4 [%0d]: %f", i, Magnitude_4[i], i, Orientation_4[i]));
+    end
+
+    sum = 0;
+    for (int i = 0; i < 9; i++) begin
+      sum += bin_1[i];
+      sum += bin_2[i];
+      sum += bin_3[i];
+      sum += bin_4[i];
+    end
+
+    for (int i = 0; i < 9; i++) begin
+      temp_fea_a[i] = $sqrt(bin_1[i] / sum);
+      temp_fea_b[i] = $sqrt(bin_2[i] / sum);
+      temp_fea_c[i] = $sqrt(bin_3[i] / sum);
+      temp_fea_d[i] = $sqrt(bin_4[i] / sum);
+    end
+
+    q_fea_a_golden.push_back(temp_fea_a);
+    q_fea_b_golden.push_back(temp_fea_b);
+    q_fea_c_golden.push_back(temp_fea_c);
+    q_fea_d_golden.push_back(temp_fea_d);
+
+    
   endfunction
 
   function void build_phase(uvm_phase phase);
@@ -254,11 +305,11 @@ class base_scoreboard extends uvm_scoreboard;
       // Tinh toan feature de so sanh
       // if (item.addr % 40 != 0) begin
       `uvm_info(get_type_name(), "Feature valid message", UVM_LOW)
-      bin_1 = fifo[41];
-      bin_2 = fifo[40];
-      bin_3 = fifo[1];
-      bin_4 = fifo[0];
-      extract_feature(bin_1, bin_2, bin_3, bin_4);
+      data_1 = fifo[41];
+      data_2 = fifo[40];
+      data_3 = fifo[1];
+      data_4 = fifo[0];
+      extract_feature(data_1, data_2, data_3, data_4);
       // end
       cnt = cnt - 1;
     end
@@ -268,7 +319,7 @@ class base_scoreboard extends uvm_scoreboard;
   virtual function void write_mon(base_item item);
     `uvm_info(get_type_name(), $sformatf("Captured packet from mon %s", item.sprint()), UVM_LOW)
     // for (int i = 0; i < 288; i = i + 32) begin
-    //   temp_fea_a[i/32] = 1.0 * (item.fea_a[i+:32]) / 2 ** 28;  //1.0*(bin_1[i +: 32]) / 2**16
+    //   temp_fea_a[i/32] = 1.0 * (item.fea_a[i+:32]) / 2 ** 28;  //1.0*(data_1[i +: 32]) / 2**16
     //   temp_fea_b[i/32] = 1.0 * (item.fea_b[i+:32]) / 2 ** 28;
     //   temp_fea_c[i/32] = 1.0 * (item.fea_c[i+:32]) / 2 ** 28;
     //   temp_fea_d[i/32] = 1.0 * (item.fea_d[i+:32]) / 2 ** 28;
